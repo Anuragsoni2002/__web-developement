367d2a041f3eb5f6f75becf4ec675ea9d04000df
Anurag Soni <137990100+Anuragsoni2002@users.noreply.github.com>
2023-08-02 17:39:06 +0530

    Add files via upload

---
+++ Assignment 08 , While loop/Q3 , Solution.py
@@ -0,0 +1,19 @@
+"""
+  Write a python script to print first 10 natural numbers in reverse order.
+i=10
+while i:
+    print(i)
+    i-=1
+
+
+i=10
+while i>0:
+    print(i)
+    i-=1
+
+"""
+
+i=10
+while i>=1:
+    print(i)
+    i-=1
